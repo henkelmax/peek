a49a6c66dc40ab8028b2bfce3d9473e245c18daa
Max Henkel <max@maxhenkel.de>
2023-07-11 17:20:57 +0200

    Add Shulker Box block hint

--- src/main/java/de/maxhenkel/peek/mixin/ShulkerBoxBlockMixin.java
+++ src/main/java/de/maxhenkel/peek/mixin/ShulkerBoxBlockMixin.java
@@ -1,19 +1,45 @@
 package de.maxhenkel.peek.mixin;
 
 import de.maxhenkel.peek.Peek;
+import de.maxhenkel.peek.data.DataStore;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.ShulkerBoxBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ShulkerBoxBlock.class)
 public class ShulkerBoxBlockMixin {
 
     @Inject(method = "appendHoverText", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/BaseEntityBlock;appendHoverText(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/BlockGetter;Ljava/util/List;Lnet/minecraft/world/item/TooltipFlag;)V", shift = At.Shift.AFTER), cancellable = true)
-    private void injected(CallbackInfo ci) {
+    private void appendHoverText(CallbackInfo ci) {
         if (Peek.CLIENT_CONFIG.peekShulkerBoxes.get()) {
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "use", at = @At(value = "HEAD"))
+    private void use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+        if (!Peek.CLIENT_CONFIG.showShulkerBoxBlockHint.get()) {
+            return;
+        }
+
+        if (!level.isClientSide) {
+            return;
+        }
+        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        if (blockEntity instanceof ShulkerBoxBlockEntity shulkerBoxBlockEntity) {
+            DataStore.lastOpenedShulkerBox = shulkerBoxBlockEntity;
         }
     }
 
---
+++ src/main/java/de/maxhenkel/peek/mixin/ShulkerBoxRendererMixin.java
@@ -0,0 +1,28 @@
+package de.maxhenkel.peek.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import de.maxhenkel.peek.Peek;
+import de.maxhenkel.peek.events.RenderEvents;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.blockentity.ShulkerBoxRenderer;
+import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ShulkerBoxRenderer.class)
+public class ShulkerBoxRendererMixin {
+
+    @Inject(method = "render", at = @At(value = "TAIL"))
+    private void render(ShulkerBoxBlockEntity shulkerBoxBlockEntity, float partialTicks, PoseStack poseStack, MultiBufferSource multiBufferSource, int light, int overlay, CallbackInfo ci) {
+        if (!Peek.CLIENT_CONFIG.showShulkerBoxBlockHint.get()) {
+            return;
+        }
+
+        if (!shulkerBoxBlockEntity.isEmpty()) {
+            RenderEvents.renderShulkerBoxLabel(shulkerBoxBlockEntity, partialTicks, poseStack, multiBufferSource, light, overlay);
+        }
+    }
+
+}
