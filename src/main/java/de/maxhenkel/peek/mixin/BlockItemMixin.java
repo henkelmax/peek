fbbf12b0241c3519785c1cd9598a3abdc24aa6a0
Max Henkel <max@maxhenkel.de>
2023-07-11 17:27:41 +0200

    Clean up code
    (cherry picked from commit b272cf3f2c99e246e2726175de032c4327fcf8af)

--- src/main/java/de/maxhenkel/peek/mixin/BlockItemMixin.java
+++ src/main/java/de/maxhenkel/peek/mixin/BlockItemMixin.java
@@ -1,17 +1,11 @@
 package de.maxhenkel.peek.mixin;
 
-import de.maxhenkel.peek.Peek;
-import de.maxhenkel.peek.tooltips.ContainerTooltip;
-import net.minecraft.core.NonNullList;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.Tag;
-import net.minecraft.world.ContainerHelper;
+import de.maxhenkel.peek.events.TooltipImageEvents;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,8 +14,6 @@ import java.util.Optional;
 
 @Mixin(BlockItem.class)
 public abstract class BlockItemMixin extends Item {
-
-    private static final String ITEMS = "Items";
 
     @Shadow
     @Final
@@ -33,122 +25,10 @@ public abstract class BlockItemMixin extends Item {
 
     @Override
     public Optional<TooltipComponent> getTooltipImage(ItemStack stack) {
-        if (block instanceof ShulkerBoxBlock) {
-            return getShulkerBoxTooltipImage(stack);
-        } else if (block instanceof ChestBlock) {
-            return getChestTooltipImage(stack);
-        } else if (block instanceof BarrelBlock) {
-            return getBarrelTooltipImage(stack);
-        } else if (block instanceof DispenserBlock) {
-            return getDispenserTooltipImage(stack);
-        } else if (block instanceof HopperBlock) {
-            return getHopperTooltipImage(stack);
+        Optional<TooltipComponent> tooltipImage = TooltipImageEvents.getTooltipImage(stack, block);
+        if (tooltipImage == null) {
+            return super.getTooltipImage(stack);
         }
-
-        return super.getTooltipImage(stack);
+        return tooltipImage;
     }
-
-    private Optional<TooltipComponent> getShulkerBoxTooltipImage(ItemStack stack) {
-        if (!Peek.CLIENT_CONFIG.peekShulkerBoxes.get()) {
-            return super.getTooltipImage(stack);
-        }
-
-        CompoundTag blockEntityData = BlockItem.getBlockEntityData(stack);
-        if (blockEntityData == null) {
-            return super.getTooltipImage(stack);
-        }
-
-        if (!blockEntityData.contains(ShulkerBoxBlockEntity.ITEMS_TAG, Tag.TAG_LIST)) {
-            return super.getTooltipImage(stack);
-        }
-
-        NonNullList<ItemStack> items = NonNullList.withSize(27, ItemStack.EMPTY);
-        ContainerHelper.loadAllItems(blockEntityData, items);
-
-        if (!Peek.CLIENT_CONFIG.showEmptyContainers.get() && items.stream().allMatch(ItemStack::isEmpty)) {
-            return super.getTooltipImage(stack);
-        }
-
-        return Optional.of(new ContainerTooltip(9, 3, items));
-    }
-
-    private Optional<TooltipComponent> getChestTooltipImage(ItemStack stack) {
-        if (!Peek.CLIENT_CONFIG.peekChests.get()) {
-            return super.getTooltipImage(stack);
-        }
-        return getDefaultChestSizeTooltipImage(stack);
-    }
-
-    private Optional<TooltipComponent> getBarrelTooltipImage(ItemStack stack) {
-        if (!Peek.CLIENT_CONFIG.peekBarrels.get()) {
-            return super.getTooltipImage(stack);
-        }
-        return getDefaultChestSizeTooltipImage(stack);
-    }
-
-    private Optional<TooltipComponent> getDispenserTooltipImage(ItemStack stack) {
-        if (!Peek.CLIENT_CONFIG.peekDispensers.get()) {
-            return super.getTooltipImage(stack);
-        }
-
-        CompoundTag blockEntityData = BlockItem.getBlockEntityData(stack);
-        if (blockEntityData == null) {
-            return super.getTooltipImage(stack);
-        }
-        if (!blockEntityData.contains(ITEMS, Tag.TAG_LIST)) {
-            return super.getTooltipImage(stack);
-        }
-
-        NonNullList<ItemStack> items = NonNullList.withSize(9, ItemStack.EMPTY);
-        ContainerHelper.loadAllItems(blockEntityData, items);
-
-        if (!Peek.CLIENT_CONFIG.showEmptyContainers.get() && items.stream().allMatch(ItemStack::isEmpty)) {
-            return super.getTooltipImage(stack);
-        }
-
-        return Optional.of(new ContainerTooltip(3, 3, items));
-    }
-
-    private Optional<TooltipComponent> getHopperTooltipImage(ItemStack stack) {
-        if (!Peek.CLIENT_CONFIG.peekHoppers.get()) {
-            return super.getTooltipImage(stack);
-        }
-
-        CompoundTag blockEntityData = BlockItem.getBlockEntityData(stack);
-        if (blockEntityData == null) {
-            return super.getTooltipImage(stack);
-        }
-        if (!blockEntityData.contains(ITEMS, Tag.TAG_LIST)) {
-            return super.getTooltipImage(stack);
-        }
-
-        NonNullList<ItemStack> items = NonNullList.withSize(5, ItemStack.EMPTY);
-        ContainerHelper.loadAllItems(blockEntityData, items);
-
-        if (!Peek.CLIENT_CONFIG.showEmptyContainers.get() && items.stream().allMatch(ItemStack::isEmpty)) {
-            return super.getTooltipImage(stack);
-        }
-
-        return Optional.of(new ContainerTooltip(5, 1, items));
-    }
-
-    private Optional<TooltipComponent> getDefaultChestSizeTooltipImage(ItemStack stack) {
-        CompoundTag blockEntityData = BlockItem.getBlockEntityData(stack);
-        if (blockEntityData == null) {
-            return super.getTooltipImage(stack);
-        }
-        if (!blockEntityData.contains(ITEMS, Tag.TAG_LIST)) {
-            return super.getTooltipImage(stack);
-        }
-
-        NonNullList<ItemStack> items = NonNullList.withSize(27, ItemStack.EMPTY);
-        ContainerHelper.loadAllItems(blockEntityData, items);
-
-        if (!Peek.CLIENT_CONFIG.showEmptyContainers.get() && items.stream().allMatch(ItemStack::isEmpty)) {
-            return super.getTooltipImage(stack);
-        }
-
-        return Optional.of(new ContainerTooltip(9, 3, items));
-    }
-
 }
