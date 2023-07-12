8b7be8004cec788ecc718ac0ab31bd8b291d5515
Max Henkel <max@maxhenkel.de>
2023-07-12 09:52:54 +0200

    Reorder config
    (cherry picked from commit 362594d030968030dec09b20c523a449f0810bba)

--- src/main/java/de/maxhenkel/peek/config/ClientConfig.java
+++ src/main/java/de/maxhenkel/peek/config/ClientConfig.java
@@ -6,8 +6,7 @@ import de.maxhenkel.configbuilder.ConfigEntry;
 public class ClientConfig {
 
     public final ConfigEntry<Boolean> showEmptyContainers;
-    public final ConfigEntry<Boolean> showShulkerBoxItemHint;
-    public final ConfigEntry<Boolean> showShulkerBoxBlockHint;
+
     public final ConfigEntry<Boolean> peekShulkerBoxes;
     public final ConfigEntry<Boolean> peekChests;
     public final ConfigEntry<Boolean> peekBarrels;
@@ -19,10 +18,12 @@ public class ClientConfig {
     public final ConfigEntry<Boolean> peekRecoveryCompasses;
     public final ConfigEntry<Boolean> peekSuspiciousStews;
 
+    public final ConfigEntry<Boolean> showShulkerBoxItemHint;
+    public final ConfigEntry<Boolean> showShulkerBoxBlockHint;
+
     public ClientConfig(ConfigBuilder builder) {
         showEmptyContainers = builder.booleanEntry("show_empty_containers", false);
-        showShulkerBoxItemHint = builder.booleanEntry("show_shulker_box_item_hint", true);
-        showShulkerBoxBlockHint = builder.booleanEntry("show_shulker_box_block_hint", true);
+
         peekShulkerBoxes = builder.booleanEntry("peek_shulker_boxes", true);
         peekChests = builder.booleanEntry("peek_chests", true);
         peekBarrels = builder.booleanEntry("peek_barrels", true);
@@ -33,6 +34,9 @@ public class ClientConfig {
         peekCompasses = builder.booleanEntry("peek_compasses", true);
         peekRecoveryCompasses = builder.booleanEntry("peek_recovery_compasses", true);
         peekSuspiciousStews = builder.booleanEntry("peek_suspicious_stews", true);
+
+        showShulkerBoxItemHint = builder.booleanEntry("show_shulker_box_item_hint", true);
+        showShulkerBoxBlockHint = builder.booleanEntry("show_shulker_box_block_hint", true);
     }
 
 }
