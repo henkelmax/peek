4c89ea7a1a9f194a10ec61dca45ec36d1872b428
Max Henkel <max@maxhenkel.de>
2023-07-12 15:30:48 +0200

    Improve shulker box data string format
    (cherry picked from commit 198580462a8199ea9c9f70fc44a356bfe4543125)

--- src/main/java/de/maxhenkel/peek/utils/ShulkerBoxUtils.java
+++ src/main/java/de/maxhenkel/peek/utils/ShulkerBoxUtils.java
@@ -13,16 +13,11 @@ import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
 
 import javax.annotation.Nullable;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ShulkerBoxUtils {
-
-    private static final Pattern ITEM_ID_PATTERN = Pattern.compile("\\{((?:[a-z0-9_-]+:)?[a-z0-9_-]+)}");
 
     public static NonNullList<ItemStack> getItems(ItemStack stack) {
         NonNullList<ItemStack> items = NonNullList.withSize(27, ItemStack.EMPTY);
@@ -90,55 +85,12 @@ public class ShulkerBoxUtils {
     }
 
     @Nullable
-    private static Item getItemFromName(@Nullable String name) {
-        if (name == null) {
-            return null;
-        }
+    public static Item byId(String id) {
         ClientLevel level = Minecraft.getInstance().level;
         if (level == null) {
             return null;
         }
-        Matcher matcher = ITEM_ID_PATTERN.matcher(name);
-        if (!matcher.find()) {
-            return null;
-        }
-        String group = matcher.group(1);
-        ResourceLocation id = new ResourceLocation(group);
-        return level.registryAccess().registryOrThrow(Registries.ITEM).get(id);
-    }
-
-    public static ShulkerHintData getHint(NonNullList<ItemStack> contents, @Nullable Component name) {
-        ShulkerHintData data = new ShulkerHintData();
-
-        String text = getStringFromComponent(name);
-
-        Item itemFromName = getItemFromName(text);
-        if (itemFromName != null) {
-            if (itemFromName != Items.AIR) {
-                data.setDisplayItem(itemFromName);
-            }
-        } else {
-            data.setDisplayItem(getBulkItem(contents));
-        }
-
-        if (text != null) {
-            data.setLabel(cleanName(name));
-        }
-
-        return data;
-    }
-
-    @Nullable
-    public static Component cleanName(@Nullable Component component) {
-        if (component == null) {
-            return null;
-        }
-        Matcher matcher = ITEM_ID_PATTERN.matcher(component.getString());
-        if (matcher.find()) {
-            String replaced = matcher.replaceAll("").trim();
-            return replaced.isEmpty() ? null : Component.literal(replaced);
-        }
-        return component;
+        return level.registryAccess().registryOrThrow(Registries.ITEM).get(new ResourceLocation(id));
     }
 
 }
--- src/main/java/de/maxhenkel/peek/utils/ShulkerHintData.java
+++ src/main/java/de/maxhenkel/peek/utils/ShulkerHintData.java
@@ -1,9 +1,15 @@
 package de.maxhenkel.peek.utils;
 
+import net.minecraft.core.NonNullList;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ShulkerHintData {
 
@@ -38,4 +44,59 @@ public class ShulkerHintData {
     public void setDisplayItem(@Nullable Item displayItem) {
         this.displayItem = displayItem;
     }
+
+    private static final Pattern DATA_PATTERN = Pattern.compile("\\{([^{}]+)}");
+
+    public static ShulkerHintData fromShulkerBox(NonNullList<ItemStack> contents, @Nullable Component name) {
+        ShulkerHintData data = ShulkerHintData.fromDataString(ShulkerBoxUtils.getStringFromComponent(name));
+        if (data != null) {
+            return data;
+        }
+        return fromContents(contents, name);
+    }
+
+    public static ShulkerHintData fromContents(NonNullList<ItemStack> contents, @Nullable Component name) {
+        ShulkerHintData data = new ShulkerHintData();
+        data.setDisplayItem(ShulkerBoxUtils.getBulkItem(contents));
+        data.setLabel(name);
+        return data;
+    }
+
+    @Nullable
+    public static ShulkerHintData fromDataString(@Nullable String s) {
+        if (s == null) {
+            return null;
+        }
+        Matcher matcher = DATA_PATTERN.matcher(s);
+        if (!matcher.find()) {
+            return null;
+        }
+        ShulkerHintData data = new ShulkerHintData();
+        String str = matcher.group(1);
+        Map<String, String> map = parseData(str);
+        if (map.containsKey("label")) {
+            data.setLabel(Component.literal(map.get("label")));
+        }
+        if (map.containsKey("item")) {
+            Item item = ShulkerBoxUtils.byId(map.get("item"));
+            if (item != null) {
+                data.setDisplayItem(item);
+            }
+        }
+        return data;
+    }
+
+    private static Map<String, String> parseData(String s) {
+        Map<String, String> data = new HashMap<>();
+        String[] split = s.split(";");
+        for (String str : split) {
+            String[] split2 = str.split("=", 2);
+            if (split2.length != 2) {
+                continue;
+            }
+            data.put(split2[0], split2[1]);
+        }
+        return data;
+    }
+
 }
