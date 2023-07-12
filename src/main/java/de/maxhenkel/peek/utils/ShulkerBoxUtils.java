4c83a76d21fa278349ab5de00fd03e824c5bd81c
Max Henkel <max@maxhenkel.de>
2023-07-12 09:20:32 +0200

    Add custom item rendering
    (cherry picked from commit 6ce1010665df5c4d5bba228959a53adf121b8b12)

--- src/main/java/de/maxhenkel/peek/utils/ShulkerBoxUtils.java
+++ src/main/java/de/maxhenkel/peek/utils/ShulkerBoxUtils.java
@@ -1,19 +1,28 @@
 package de.maxhenkel.peek.utils;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.NonNullList;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
 
 import javax.annotation.Nullable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ShulkerBoxUtils {
+
+    private static final Pattern ITEM_ID_PATTERN = Pattern.compile("\\{((?:[a-z0-9_-]+:)?[a-z0-9_-]+)}");
 
     public static NonNullList<ItemStack> getItems(ItemStack stack) {
         NonNullList<ItemStack> items = NonNullList.withSize(27, ItemStack.EMPTY);
@@ -56,9 +65,9 @@ public class ShulkerBoxUtils {
     }
 
     @Nullable
-    public static Item getRenderItem(NonNullList<ItemStack> items) {
+    public static Item getBulkItem(NonNullList<ItemStack> contents) {
         Item renderItem = null;
-        for (ItemStack itemStack : items) {
+        for (ItemStack itemStack : contents) {
             if (itemStack.isEmpty()) {
                 continue;
             }
@@ -71,6 +80,58 @@ public class ShulkerBoxUtils {
             }
         }
         return renderItem;
+    }
+
+    public static String getStringFromComponent(@Nullable Component component) {
+        if (component == null) {
+            return null;
+        }
+        return component.getString();
+    }
+
+    @Nullable
+    private static Item getItemFromName(@Nullable String name) {
+        if (name == null) {
+            return null;
+        }
+        ClientLevel level = Minecraft.getInstance().level;
+        if (level == null) {
+            return null;
+        }
+        Matcher matcher = ITEM_ID_PATTERN.matcher(name);
+        if (!matcher.find()) {
+            return null;
+        }
+        String group = matcher.group(1);
+        ResourceLocation id = new ResourceLocation(group);
+        return level.registryAccess().registryOrThrow(Registries.ITEM).get(id);
+    }
+
+    public static ShulkerHintData getHint(NonNullList<ItemStack> contents, @Nullable Component name) {
+        ShulkerHintData data = new ShulkerHintData();
+
+        String text = getStringFromComponent(name);
+
+        Item itemFromName = getItemFromName(text);
+        if (itemFromName != null) {
+            if (itemFromName != Items.AIR) {
+                data.setDisplayItem(itemFromName);
+            }
+        } else {
+            data.setDisplayItem(getBulkItem(contents));
+        }
+
+        if (text != null) {
+            Matcher matcher = ITEM_ID_PATTERN.matcher(text);
+            if (matcher.find()) {
+                String replaced = matcher.replaceAll("").trim();
+                data.setLabel(replaced.isEmpty() ? null : Component.literal(replaced));
+            } else {
+                data.setLabel(name);
+            }
+        }
+
+        return data;
     }
 
 }
