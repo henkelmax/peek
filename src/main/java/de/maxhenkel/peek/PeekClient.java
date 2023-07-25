d0a29b18abba2c1c46a339a61da442ffc0939bb9
Max Henkel <max@maxhenkel.de>
2023-07-25 19:21:03 +0200

    Add builtin resource pack for better shulker visibility

---
+++ src/main/java/de/maxhenkel/peek/PeekClient.java
@@ -0,0 +1,20 @@
+package de.maxhenkel.peek;
+
+import de.maxhenkel.peek.interfaces.IPackRepository;
+import de.maxhenkel.peek.resourcepacks.PeekResourcePack;
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+
+public class PeekClient implements ClientModInitializer {
+
+    public static PeekResourcePack ORTHOGONAL_SHULKER_ICONS;
+
+    @Override
+    public void onInitializeClient() {
+        ORTHOGONAL_SHULKER_ICONS = new PeekResourcePack("orthogonal_shulker_icons");
+        IPackRepository repository = (IPackRepository) Minecraft.getInstance().getResourcePackRepository();
+        repository.peek$addSource((consumer) -> consumer.accept(ORTHOGONAL_SHULKER_ICONS.toPack(Component.translatable("resourcepack.peek.orthogonal_shulker_icons"))));
+    }
+
+}
