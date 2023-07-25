1737bed2355a10a23ea57eb0565bd0f9f3a544f8
Max Henkel <max@maxhenkel.de>
2023-07-25 11:59:25 +0200

    Fix config not getting created on servers

--- src/main/java/de/maxhenkel/peek/Peek.java
+++ src/main/java/de/maxhenkel/peek/Peek.java
@@ -2,22 +2,19 @@ package de.maxhenkel.peek;
 
 import de.maxhenkel.configbuilder.ConfigBuilder;
 import de.maxhenkel.peek.config.PeekConfig;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Environment(EnvType.CLIENT)
-public class Peek implements ClientModInitializer {
+public class Peek implements ModInitializer {
 
     public static final String MODID = "peek";
     public static final Logger LOGGER = LogManager.getLogger(MODID);
     public static PeekConfig CONFIG;
 
     @Override
-    public void onInitializeClient() {
+    public void onInitialize() {
         CONFIG = ConfigBuilder.build(FabricLoader.getInstance().getConfigDir().resolve(MODID).resolve("peek.properties"), PeekConfig::new);
     }
 }
