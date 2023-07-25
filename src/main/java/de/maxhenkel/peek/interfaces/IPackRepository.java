a166ef7faf79da4bdb174c94b4e86ae724822ede
Max Henkel <max@maxhenkel.de>
2023-07-25 19:21:03 +0200

    Add builtin resource pack for better shulker visibility
    (cherry picked from commit d0a29b18abba2c1c46a339a61da442ffc0939bb9)

---
+++ src/main/java/de/maxhenkel/peek/interfaces/IPackRepository.java
@@ -0,0 +1,9 @@
+package de.maxhenkel.peek.interfaces;
+
+import net.minecraft.server.packs.repository.RepositorySource;
+
+public interface IPackRepository {
+
+    void peek$addSource(RepositorySource source);
+
+}
---
+++ src/main/java/de/maxhenkel/peek/resourcepacks/PeekResourcePack.java
@@ -0,0 +1,120 @@
+package de.maxhenkel.peek.resourcepacks;
+
+import com.google.common.collect.ImmutableSet;
+import de.maxhenkel.peek.Peek;
+import net.minecraft.SharedConstants;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.AbstractPackResources;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
+import net.minecraft.server.packs.resources.IoSupplier;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+
+import javax.annotation.Nullable;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class PeekResourcePack extends AbstractPackResources {
+
+    public PeekResourcePack(String id) {
+        super(id, true);
+    }
+
+    public Pack toPack(Component name) {
+        Pack.ResourcesSupplier resourcesSupplier = (s) -> this;
+        Pack.Info info = Pack.readPackInfo("", resourcesSupplier);
+        if (info == null) {
+            info = new Pack.Info(name, SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES), FeatureFlagSet.of(FeatureFlags.VANILLA));
+        }
+        return Pack.create(packId(), name, false, resourcesSupplier, info, PackType.CLIENT_RESOURCES, Pack.Position.TOP, false, PackSource.BUILT_IN);
+    }
+
+    private String getPath() {
+        return "/packs/" + packId() + "/";
+    }
+
+    @Nullable
+    private InputStream get(String name) {
+        return Peek.class.getResourceAsStream(getPath() + name);
+    }
+
+    @Nullable
+    @Override
+    public IoSupplier<InputStream> getRootResource(String... strings) {
+        return getResource(String.join("/", strings));
+    }
+
+    private static String getPathFromLocation(PackType packType, ResourceLocation resourceLocation) {
+        return String.format(Locale.ROOT, "%s/%s/%s", packType.getDirectory(), resourceLocation.getNamespace(), resourceLocation.getPath());
+    }
+
+    @Nullable
+    @Override
+    public IoSupplier<InputStream> getResource(PackType packType, ResourceLocation resourceLocation) {
+        return getResource(getPathFromLocation(packType, resourceLocation));
+    }
+
+    @Nullable
+    private IoSupplier<InputStream> getResource(String path) {
+        InputStream resourceAsStream = get(path);
+        if (resourceAsStream == null) {
+            return null;
+        }
+        return () -> resourceAsStream;
+    }
+
+    @Override
+    public void listResources(PackType type, String namespace, String prefix, ResourceOutput resourceOutput) {
+        try {
+            URL url = Peek.class.getResource(getPath());
+            if (url == null) {
+                return;
+            }
+            Path namespacePath = Paths.get(url.toURI()).resolve(type.getDirectory()).resolve(namespace);
+            Path resPath = namespacePath.resolve(prefix);
+
+            if (!Files.exists(resPath)) {
+                return;
+            }
+
+            try (Stream<Path> files = Files.walk(resPath)) {
+                files.filter(path -> !Files.isDirectory(path)).forEach(path -> {
+                    ResourceLocation resourceLocation = new ResourceLocation(namespace, convertPath(path).substring(convertPath(namespacePath).length() + 1));
+                    resourceOutput.accept(resourceLocation, getResource(type, resourceLocation));
+                });
+            }
+        } catch (Exception e) {
+            Peek.LOGGER.error("Failed to list builtin pack resources", e);
+        }
+    }
+
+    private static String convertPath(Path path) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < path.getNameCount(); i++) {
+            stringBuilder.append(path.getName(i));
+            if (i < path.getNameCount() - 1) {
+                stringBuilder.append("/");
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public Set<String> getNamespaces(PackType packType) {
+        return ImmutableSet.of(ResourceLocation.DEFAULT_NAMESPACE);
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
