be003749eefe8ece43b40d6f0ba52a68055a097d
DaTrederVid <dedemacmactretre@gmail.com>
2023-08-28 18:44:50 -0400

    Add Ender Chest Peeking
    Only using cached data. No special server side support.
    
    Delete bin, remove period in PeekConfig, fix annotation in LocalPlayerMixin
    
    Implement requested changes
    
    Improve comments
    Remove boolean (replace with isScreenEnderChest)
    Improve variable names
    Remove unused imports
    Update cache on close
    +Require fabric networking api
    Use fabric api to set cache to null on login
    Make description consistent
    
    Update .gitignore
    
    Add /bin/ to .gitignore and delete /bin/ from repo
    
    Improve comments

---
+++ src/main/java/de/maxhenkel/peek/mixin/ClientPlayNetworkHandlerMixin.java
@@ -0,0 +1,36 @@
+package de.maxhenkel.peek.mixin;
+
+import de.maxhenkel.peek.data.DataStore;
+import de.maxhenkel.peek.utils.EnderChestUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.core.NonNullList;
+import net.minecraft.network.protocol.game.*;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPacketListener.class)
+public abstract class ClientPlayNetworkHandlerMixin implements ClientGamePacketListener {
+    @Shadow
+    @Final
+    private Minecraft minecraft;
+
+    @Inject(method = "handleContainerContent", at = @At("TAIL"))
+    private void onHandleContainerContent(ClientboundContainerSetContentPacket packet, CallbackInfo ci) {
+        // Intercept packet on opening Ender Chest to set contents
+        // Could ignore all but the first, but this is potentially more reliable
+        if (EnderChestUtils.isScreenEnderChest(minecraft.screen)) {
+            if (DataStore.enderChestInventory == null) {
+                DataStore.enderChestInventory = NonNullList.withSize(27, ItemStack.EMPTY);
+            }
+            for (int i = 0; i < packet.getItems().size() - 36; i++) {
+                DataStore.enderChestInventory.set(i, packet.getItems().get(i));
+            }
+        }
+    }
+}
