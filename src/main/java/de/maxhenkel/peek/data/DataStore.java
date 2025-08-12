250ed8d8685645adebbdeb13d66c1932708168cf
Max Henkel <max@maxhenkel.de>
2025-08-12 09:06:35 +0200

    Fix compatibility with Advanced Shulkerboxes Closes #49
    (cherry picked from commit b1c20f72e7b208dfbe2e68529fff33867dade239)
    
    # Conflicts:
    #	changelog.md
    #	gradle.properties
    #	src/main/java/de/maxhenkel/peek/mixin/ShulkerBoxBlockMixin.java

--- src/main/java/de/maxhenkel/peek/data/DataStore.java
+++ src/main/java/de/maxhenkel/peek/data/DataStore.java
@@ -9,6 +9,7 @@ import javax.annotation.Nullable;
 public class DataStore {
     @Nullable
     public static ShulkerBoxBlockEntity lastOpenedShulkerBox;
+    public static int lastOpenedShulkerBoxContainerId = -1;
     @Nullable
     public static NonNullList<ItemStack> enderChestInventory;
 }
