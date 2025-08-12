1b67883705aeeaa694cc7cbf0bbdf046aec08bcc
Max Henkel <max@maxhenkel.de>
2025-08-12 09:06:35 +0200

    Fix compatibility with Advanced Shulkerboxes Closes #49
    (cherry picked from commit b1c20f72e7b208dfbe2e68529fff33867dade239)
    
    # Conflicts:
    #	changelog.md
    #	gradle.properties
    #	src/main/java/de/maxhenkel/peek/mixin/ShulkerBoxBlockMixin.java
    
    # Conflicts:
    #	changelog.md
    #	gradle.properties
    #	src/main/java/de/maxhenkel/peek/mixin/ShulkerBoxBlockMixin.java

--- src/main/java/de/maxhenkel/peek/mixin/ShulkerBoxBlockMixin.java
+++ src/main/java/de/maxhenkel/peek/mixin/ShulkerBoxBlockMixin.java
@@ -1,22 +1,23 @@
 package de.maxhenkel.peek.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import de.maxhenkel.peek.Peek;
 import de.maxhenkel.peek.data.DataStore;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.ShulkerBoxBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.OptionalInt;
 
 @Mixin(ShulkerBoxBlock.class)
 public class ShulkerBoxBlockMixin {
@@ -28,19 +29,22 @@ public class ShulkerBoxBlockMixin {
         }
     }
 
-    @Inject(method = "use", at = @At(value = "HEAD"))
-    private void use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+    @WrapOperation(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;openMenu(Lnet/minecraft/world/MenuProvider;)Ljava/util/OptionalInt;"))
+    private OptionalInt use(Player instance, MenuProvider menu, Operation<OptionalInt> original, @Local(argsOnly = true) Level level, @Local(argsOnly = true) BlockPos pos, @Local(argsOnly = true) Player player) {
+        OptionalInt result = original.call(instance, menu);
         if (!Peek.CONFIG.showShulkerBoxBlockHint.get()) {
-            return;
+            return result;
         }
 
         if (!level.isClientSide) {
-            return;
+            return result;
         }
-        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        BlockEntity blockEntity = level.getBlockEntity(pos);
         if (blockEntity instanceof ShulkerBoxBlockEntity shulkerBoxBlockEntity) {
             DataStore.lastOpenedShulkerBox = shulkerBoxBlockEntity;
+            DataStore.lastOpenedShulkerBoxContainerId = result.orElse(-1);
         }
+        return result;
     }
 
 }
