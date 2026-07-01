9c6ba41365f37aa6072108cae8b345781ce1ea3c
Max Henkel <max@maxhenkel.de>
2026-07-01 13:04:41 +0200

    Add advanced sulphur cube tooltips

--- src/main/java/de/maxhenkel/peek/mixin/SulfurCubeContentMixin.java
+++ src/main/java/de/maxhenkel/peek/mixin/SulfurCubeContentMixin.java
@@ -10,16 +10,22 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.SulfurCubeArchetype;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemStackTemplate;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.SulfurCubeContent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.function.Consumer;
 
 @Mixin(SulfurCubeContent.class)
 public class SulfurCubeContentMixin {
@@ -28,14 +34,19 @@ public class SulfurCubeContentMixin {
     @Final
     private ItemStackTemplate absorbedBlockItemStack;
 
-    @WrapOperation(method = "addToTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/chat/Component;translatable(Ljava/lang/String;[Ljava/lang/Object;)Lnet/minecraft/network/chat/MutableComponent;"))
-    public MutableComponent wrapOperation(String key, Object[] args, Operation<MutableComponent> original, @Local(argsOnly = true, name = "context") Item.TooltipContext context) {
+    @Unique
+    private static final DecimalFormat ATTRIBUTE_DECIMAL_FORMAT = new DecimalFormat("#.##");
+
+    @WrapOperation(method = "addToTooltip", at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V"))
+    public void addToTooltip(Consumer<Component> instance, Object component, Operation<Void> original, @Local(argsOnly = true, name = "context") Item.TooltipContext context, @Local(argsOnly = true, name = "flag") TooltipFlag flag, @Local(name = "currentStack") ItemStack currentStack) {
         if (!Peek.CONFIG.peekSulfurCubeArchetype.get()) {
-            return original.call(key, args);
+            original.call(instance, component);
+            return;
         }
         HolderLookup.Provider registries = context.registries();
         if (registries == null) {
-            return original.call(key, args);
+            original.call(instance, component);
+            return;
         }
         HolderLookup.RegistryLookup<SulfurCubeArchetype> archetypeRegistryLookup = registries.lookupOrThrow(Registries.SULFUR_CUBE_ARCHETYPE);
         Holder.Reference<SulfurCubeArchetype> archetype = archetypeRegistryLookup.filterElements(a -> absorbedBlockItemStack.is(a.items())).listElements().findAny().orElse(null);
@@ -52,7 +63,58 @@ public class SulfurCubeContentMixin {
                 archetypeComponent = Component.literal(identifier.toString()).withStyle(ChatFormatting.DARK_GRAY);
             }
         }
-        return original.call(key, args).append(" ").append(Component.translatable("tooltip.peek.sulfur_cube_archetype", archetypeComponent));
+        instance.accept(
+                Component.translatable("entity.minecraft.sulfur_cube.content", currentStack.getHoverName())
+                        .withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC)
+                        .append(" ")
+                        .append(Component.translatable("tooltip.peek.sulfur_cube_archetype", archetypeComponent))
+        );
+
+        if (archetype != null && flag.isAdvanced()) {
+            SulfurCubeArchetype a = archetype.value();
+            instance.accept(Component.empty());
+            if (a.buoyant()) {
+                instance.accept(Component.translatable("tooltip.peek.sulfur_cube_archetype_advanced.buoyant").withStyle(ChatFormatting.GRAY));
+            }
+            a.contactDamage().ifPresent(contactDamage -> {
+                float min = contactDamage.amount().min();
+                float max = contactDamage.amount().max();
+                if (min != max) {
+                    instance.accept(Component.translatable("tooltip.peek.sulfur_cube_archetype_advanced.contact_damage_range", ATTRIBUTE_DECIMAL_FORMAT.format(min), ATTRIBUTE_DECIMAL_FORMAT.format(max)).withStyle(ChatFormatting.GRAY));
+                } else {
+                    instance.accept(Component.translatable("tooltip.peek.sulfur_cube_archetype_advanced.contact_damage", ATTRIBUTE_DECIMAL_FORMAT.format(min)).withStyle(ChatFormatting.GRAY));
+                }
+            });
+            a.explosion().ifPresent(explosion -> {
+                if (explosion.causesFire()) {
+                    instance.accept(Component.translatable("tooltip.peek.sulfur_cube_archetype_advanced.explosion_fire", explosion.power(), explosion.fuse()).withStyle(ChatFormatting.GRAY));
+                } else {
+                    instance.accept(Component.translatable("tooltip.peek.sulfur_cube_archetype_advanced.explosion", explosion.power(), explosion.fuse()).withStyle(ChatFormatting.GRAY));
+                }
+            });
+
+            SulfurCubeArchetype.KnockbackModifiers km = a.knockbackModifiers();
+            instance.accept(Component.translatable("tooltip.peek.sulfur_cube_archetype_advanced.knockback_modifiers", ATTRIBUTE_DECIMAL_FORMAT.format(km.horizontalPower()), ATTRIBUTE_DECIMAL_FORMAT.format(km.verticalPower())).withStyle(ChatFormatting.GRAY));
+            instance.accept(Component.empty());
+
+            List<SulfurCubeArchetype.AttributeEntry> attributeEntries = a.attributeModifiers();
+            if (!attributeEntries.isEmpty()) {
+                instance.accept(Component.translatable("tooltip.peek.sulfur_cube_archetype_advanced.attributes").append(":").withStyle(ChatFormatting.GRAY));
+            }
+            attributeEntries.forEach(attribute -> {
+                instance.accept(
+                        Component.literal("  ")
+                                .append(attribute.attribute().getRegisteredName())
+                                .append(": ")
+                                .append(ATTRIBUTE_DECIMAL_FORMAT.format(attribute.modifier().amount()))
+                                .withStyle(ChatFormatting.GRAY)
+                );
+            });
+
+            if (!attributeEntries.isEmpty()) {
+                instance.accept(Component.empty());
+            }
+        }
     }
 
 }
